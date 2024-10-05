0959ed781c591458483e42b78f785f8c07150191
Jokir <doner24072004@gmail.com>
2024-10-05 12:19:09 +0300

    Added Layers

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -9,53 +9,63 @@ import * as L from 'leaflet';
 })
 export class MapComponent implements OnInit {
   private map: any;
+  private cities = L.layerGroup();
 
   ngOnInit(): void {
-    console.log('hi');
     this.initMap();
+    this.loadCustomLayer();
   }
 
   private initMap(): void {
-    this.map = L.map('map').setView([51.505, -0.09], 13);
-
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    const osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
-      attribution:
-        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    }).addTo(this.map);
-
-    const marker = L.marker([50.446355, 30.467319])
-      .addTo(this.map)
-      .bindPopup('<b>Hello world!</b><br />I am a popup.')
-      .openPopup();
-
-    const circle = L.circle([51.508, -0.11], {
-      color: 'red',
-      fillColor: '#f03',
-      fillOpacity: 0.5,
-      radius: 500,
-    })
-      .addTo(this.map)
-      .bindPopup('I am a circle.');
-
-    const polygon = L.polygon([
-      [51.509, -0.08],
-      [51.503, -0.06],
-      [51.51, -0.047],
-    ])
-      .addTo(this.map)
-      .bindPopup('I am a polygon.');
-
-    const popup = L.popup()
-      .setLatLng([51.513, -0.09])
-      .setContent('I am a standalone popup.')
-      .openOn(this.map);
-
-    this.map.on('click', (e: any) => {
-      popup
-        .setLatLng(e.latlng)
-        .setContent(`You clicked the map at ${e.latlng.toString()}`)
-        .openOn(this.map);
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     });
+
+    const osmHOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a>'
+    });
+
+    this.map = L.map('map', {
+      center: [39.73, -104.99],
+      zoom: 10,
+      layers: [osm, this.cities]
+    });
+
+    const baseLayers = {
+      'OpenStreetMap': osm,
+      'OpenStreetMap.HOT': osmHOT
+    };
+
+    const overlays = {
+      'Cities': this.cities
+    };
+
+    const layerControl = L.control.layers(baseLayers, overlays).addTo(this.map);
+
+    const openTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+    layerControl.addBaseLayer(openTopoMap, 'OpenTopoMap');
+
+    // Adding cities layer markers
+    L.marker([39.61, -105.02]).bindPopup('This is Littleton, CO.').addTo(this.cities);
+    L.marker([39.74, -104.99]).bindPopup('This is Denver, CO.').addTo(this.cities);
+    L.marker([39.73, -104.8]).bindPopup('This is Aurora, CO.').addTo(this.cities);
+    L.marker([39.77, -105.23]).bindPopup('This is Golden, CO.').addTo(this.cities);
+
+    // Adding parks layer
+    const parks = L.layerGroup([
+      L.marker([39.75, -105.09]).bindPopup('This is Crown Hill Park.'),
+      L.marker([39.68, -105.00]).bindPopup('This is Ruby Hill Park.')
+    ]);
+
+    layerControl.addOverlay(parks, 'Parks');
+  }
+
+  loadCustomLayer() {
+    const customLayer = L.geoJSON()
   }
 }
